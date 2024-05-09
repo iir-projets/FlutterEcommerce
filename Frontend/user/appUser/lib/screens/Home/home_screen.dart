1ebe45c807c20b44780aee7dd8d4c869b55f3786
amine10speed <121752633+amine10speed@users.noreply.github.com>
2024-05-09 18:25:27 +0100

    changeA7
    responsive home

--- Frontend/user/appUser/lib/screens/Home/home_screen.dart
+++ Frontend/user/appUser/lib/screens/Home/home_screen.dart
@@ -51,17 +51,15 @@ class _HomeScreenState extends State<HomeScreen> {
             backgroundColor: Colors.white,
             body: SingleChildScrollView(
               child: Padding(
-                padding: EdgeInsets.all(Responsive.isMobile(context) ? 10 : 20),
+                padding: EdgeInsets.all(Responsive.screenWidth(context) < 600 ? 10 : 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(height: Responsive.isMobile(context) ? 20 : 35),
-                    // for custom appbar
+                    SizedBox(height: Responsive.screenWidth(context) < 600 ? 20 : 35),
                     const CustomAppBar(),
-                    SizedBox(height: Responsive.isMobile(context) ? 10 : 20),
-                    // for search bar
+                    SizedBox(height: Responsive.screenWidth(context) < 600 ? 10 : 20),
                     const MySearchBAR(),
-                    SizedBox(height: Responsive.isMobile(context) ? 10 : 20),
+                    SizedBox(height: Responsive.screenWidth(context) < 600 ? 10 : 20),
                     ImageSlider(
                       currentSlide: currentSlider,
                       onChange: (value) {
@@ -72,11 +70,9 @@ class _HomeScreenState extends State<HomeScreen> {
                         );
                       },
                     ),
-                    SizedBox(height: Responsive.isMobile(context) ? 10 : 20),
-                    // for category selection
+                    SizedBox(height: Responsive.screenWidth(context) < 600 ? 10 : 20),
                     categoryItems(),
-
-                    SizedBox(height: Responsive.isMobile(context) ? 10 : 20),
+                    SizedBox(height: Responsive.screenWidth(context) < 600 ? 10 : 20),
                     if (selectedIndex == 0)
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -84,7 +80,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           Text(
                             "Special For You",
                             style: TextStyle(
-                              fontSize: Responsive.isMobile(context) ? 18 : 25,
+                              fontSize: Responsive.isDesktop(context) ? 25 : 18,
                               fontWeight: FontWeight.w800,
                             ),
                           ),
@@ -92,21 +88,20 @@ class _HomeScreenState extends State<HomeScreen> {
                             "See all",
                             style: TextStyle(
                               fontWeight: FontWeight.w500,
-                              fontSize: Responsive.isMobile(context) ? 12 : 16,
+                              fontSize: Responsive.isDesktop(context) ? 16 : 12,
                               color: Colors.black54,
                             ),
                           ),
                         ],
                       ),
-                    // for shopping items
-                    SizedBox(height: Responsive.isMobile(context) ? 5 : 10),
+                    SizedBox(height: Responsive.isDesktop(context) ? 10 : 5),
                     GridView.builder(
                       padding: EdgeInsets.zero,
                       physics: const NeverScrollableScrollPhysics(),
                       shrinkWrap: true,
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: Responsive.isMobile(context) ? 1 : 2,
-                          childAspectRatio: Responsive.isMobile(context) ? 1.2 : 0.75,
+                          crossAxisCount: Responsive.isDesktop(context) ? 4 : 2,
+                          childAspectRatio: Responsive.isDesktop(context) ? 1 : 0.75,
                           crossAxisSpacing: 20,
                           mainAxisSpacing: 20),
                       itemCount: selectcategories[selectedIndex].length,
@@ -130,8 +125,7 @@ class _HomeScreenState extends State<HomeScreen> {
         scrollDirection: Axis.horizontal,
         itemCount: categoriesList.length,
         physics: const BouncingScrollPhysics(),
-        separatorBuilder: (context, index) =>
-            const SizedBox(width: 30), // space between each element
+        separatorBuilder: (context, index) => const SizedBox(width: 30),
         itemBuilder: (context, index) {
           return GestureDetector(
             onTap: () {
@@ -143,9 +137,7 @@ class _HomeScreenState extends State<HomeScreen> {
               padding: const EdgeInsets.all(8),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
-                color: selectedIndex == index
-                    ? Colors.grey[200]
-                    : Colors.transparent,
+                color: selectedIndex == index ? Colors.grey[200] : Colors.transparent,
               ),
               child: Column(
                 children: [
@@ -159,14 +151,13 @@ class _HomeScreenState extends State<HomeScreen> {
                         fit: BoxFit.cover,
                       ),
                     ),
-                    padding:
-                        const EdgeInsets.only(bottom: 5), // padding at the bottom
+                    padding: const EdgeInsets.only(bottom: 5),
                   ),
-                  SizedBox(height: Responsive.isMobile(context) ? 3 : 6),
+                  SizedBox(height: Responsive.isDesktop(context) ? 6 : 3),
                   Text(
                     categoriesList[index].title,
                     style: TextStyle(
-                      fontSize: Responsive.isMobile(context) ? 14 : 16,
+                      fontSize: Responsive.isDesktop(context) ? 16 : 14,
                       fontWeight: FontWeight.bold,
                     ),
                   )

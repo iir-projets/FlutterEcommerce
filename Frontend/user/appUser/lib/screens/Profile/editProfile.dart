179e276781ff66527b4456285ccf62fb6317c301
Safaamigo <safaabatrahi7@gmail.com>
2024-05-09 13:22:14 +0100

    changeS6

--- Frontend/user/appUser/lib/screens/Profile/editProfile.dart
+++ Frontend/user/appUser/lib/screens/Profile/editProfile.dart
@@ -42,28 +42,35 @@ class ModifyProfileScreen extends StatelessWidget {
                   alignment: Alignment.center,
                   children: [
                     CircleAvatar(
-                      radius: 80,
+                      radius: 50,
                       backgroundColor: kprimaryColor,
-                      child: Text('A',
+                      child: Text('Profile',
                           style: TextStyle(
-                              fontSize: 80,
+                              fontSize: 20,
                               color: Colors
                                   .white)), // Placeholder for profile image
                     ),
-                    Positioned(
-                      right: 0,
-                      bottom: 0,
-                      child: IconButton(
-                        icon: Icon(Icons.edit, color: priceColor),
-                        onPressed: () {},
-                      ),
-                    ),
+                    // Positioned(
+                    //   right: 0,
+                    //   bottom: 0,
+                    //   child: IconButton(
+                    //     icon: Icon(Icons.edit, color: priceColor),
+                    //     onPressed: () {},
+                    //   ),
+                    // ),
                   ],
                 ),
                 SizedBox(height: 30),
                 TextField(
                   decoration: InputDecoration(
-                    labelText: 'Name',
+                    labelText: 'Nom',
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Prénom',
                     prefixIcon: Icon(Icons.person),
                   ),
                 ),
@@ -79,6 +86,13 @@ class ModifyProfileScreen extends StatelessWidget {
                   decoration: InputDecoration(
                     labelText: 'Phone',
                     prefixIcon: Icon(Icons.phone),
+                  ),
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Adresse',
+                    prefixIcon: Icon(Icons.location_on),
                   ),
                 ),
                 SizedBox(height: 30),

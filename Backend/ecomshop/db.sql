f225266ce7750ea320806f859b2bf730736727e8
amine10speed <amine8el@gmail.com>
2024-05-09 15:26:09 +0100

    db

--- Backend/ecomshop/db.sql
+++ Backend/ecomshop/db.sql
@@ -1,50 +1,184 @@
--- MySQL Script to create the database and tables
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 09, 2024 at 04:14 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
--- Optional: Create or use an existing database
-CREATE DATABASE IF NOT EXISTS ecom;
-USE ecom;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Create the `user` table
-CREATE TABLE `user` (
-    UserId INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    prenom VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    telephone VARCHAR(255),
-    address VARCHAR(255),
-    password VARCHAR(255) NOT NULL
-);
 
--- Create the `categorie` table
-CREATE TABLE `categorie` (
-    catId INT AUTO_INCREMENT PRIMARY KEY,
-    catNom VARCHAR(255) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Create the `article` table
+--
+-- Database: `ecom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article`
+--
+
 CREATE TABLE `article` (
-    articleId INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    desc TEXT,
-    price DOUBLE NOT NULL,
-    image BLOB
-);
+  `articleId` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `desc` text DEFAULT NULL,
+  `price` double NOT NULL,
+  `image` varchar(255) DEFAULT NULL, 
+  PRIMARY KEY (`articleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Create the `transaction` table
-CREATE TABLE `transaction` (
-    transactionId INT AUTO_INCREMENT PRIMARY KEY,
-    transactionDate DATE NOT NULL,
-    transactionStatus VARCHAR(255) NOT NULL,
-    UserId INT,
-    FOREIGN KEY (UserId) REFERENCES user(UserId)
-);
 
--- Create the `detail-commande` table
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `catId` int(11) NOT NULL,
+  `catNom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail-commande`
+--
+
 CREATE TABLE `detail-commande` (
-    detailId INT AUTO_INCREMENT PRIMARY KEY,
-    articleId INT NOT NULL,
-    transactionId INT NOT NULL,
-    Qty INT NOT NULL,
-    FOREIGN KEY (articleId) REFERENCES article(articleId),
-    FOREIGN KEY (transactionId) REFERENCES transaction(transactionId)
-);
+  `detailId` int(11) NOT NULL,
+  `articleId` int(11) NOT NULL,
+  `transactionId` int(11) NOT NULL,
+  `Qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `transactionId` int(11) NOT NULL,
+  `transactionDate` date NOT NULL,
+  `transactionStatus` varchar(255) NOT NULL,
+  `UserId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserId` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telephone` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`articleId`);
+
+--
+-- Indexes for table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`catId`);
+
+--
+-- Indexes for table `detail-commande`
+--
+ALTER TABLE `detail-commande`
+  ADD PRIMARY KEY (`detailId`),
+  ADD KEY `articleId` (`articleId`),
+  ADD KEY `transactionId` (`transactionId`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transactionId`),
+  ADD KEY `UserId` (`UserId`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `article`
+--
+ALTER TABLE `article`
+  MODIFY `articleId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `catId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detail-commande`
+--
+ALTER TABLE `detail-commande`
+  MODIFY `detailId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail-commande`
+--
+ALTER TABLE `detail-commande`
+  ADD CONSTRAINT `detail-commande_ibfk_1` FOREIGN KEY (`articleId`) REFERENCES `article` (`articleId`),
+  ADD CONSTRAINT `detail-commande_ibfk_2` FOREIGN KEY (`transactionId`) REFERENCES `transaction` (`transactionId`);
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

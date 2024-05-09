d8c46eb91fd5d3e228907b8f3891abe365ba03c5
Safaamigo <safaabatrahi7@gmail.com>
2024-05-09 17:59:57 +0100

    changeS4

---
+++ Backend/ecomshop/src/main/java/com/ecom/ecomshop/repository/AdminRepository.java
@@ -0,0 +1,12 @@
+package com.ecom.ecomshop.repository;
+
+import com.ecom.ecomshop.model.Admin;
+
+public class AdminRepository {
+
+    public Admin findByUsernameAndPassword(String username, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByUsernameAndPassword'");
+    }
+    
+}
